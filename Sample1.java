6502a884d3aeb3d30bc4b9f08304478917806e60
ymjfakruddin <jeelanfakruddin@gmail.com>
2022-01-31 08:40:27 +0530

    In IF/ElSE  we don't add return statement will not get correct output

---
+++ Sample1.java
@@ -0,0 +1,23 @@
+class  Sample1
+{
+	public static void main(String[] args) 
+	{
+          int x = 110;
+		  int y = 22;
+		if (x > y)
+		{
+			System.out.println("x is greater than y	");
+			return;
+	     }
+		if(x < y)
+		{
+			System.out.println("x is less than y");
+			return;
+			
+		}
+		else
+		{
+			System.out.println("check the whether u entered Int or not");
+		}
+}
+}
