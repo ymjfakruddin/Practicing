3d36ee7f3e49ae10d4660e0a73a89d2a1d455213
ymjfakruddin <jeelanfakruddin@gmail.com>
2022-01-31 09:21:15 +0530

    It is Bypass condition

---
+++ Sample2.java
@@ -0,0 +1,31 @@
+class  Sample2
+{
+	public static void main(String[] args) 
+	{
+	
+          int x = 110;
+		  int y = 22;
+		if (x < y)
+		{
+			System.out.println("x is lesser (IF) than y	");
+			//return;
+	     }
+	   else if (x <y)
+	   {
+          System.out.println("x is less than y");
+	   }
+	   else if(x >y || x == y)
+		{
+		   System.out.println("x is greater than y or x is equal to y");
+		}
+		else if(x >y)
+		{
+			System.out.println("X is greater than y");
+		}
+		else
+		{
+			System.out.println("please enter a correct output");
+		}
+
+	}
+}
