41b9631a1d0193c1bf086f6f706585048632e57f
ymjfakruddin <jeelanfakruddin@gmail.com>
2022-02-11 22:04:46 +0530

    B.java

---
+++ B.java
@@ -0,0 +1,17 @@
+class B 
+{
+	public static void main(String[] args) 
+	{
+		Node n1 = new Node();
+		System.out.println(n1.num);
+	}
+}
+class Node
+{
+	int num=10;
+	Node left;
+	public Node()
+	{
+       left = "jeelan";
+	}
+}
