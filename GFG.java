71d7c807b92d6b82094234f0dc8450be51c6a3fe
ymjfakruddin <jeelanfakruddin@gmail.com>
2022-02-15 19:41:53 +0530

    GFG - Call By reference Example

---
+++ GFG.java
@@ -0,0 +1,20 @@
+class GFG 
+{
+
+	public static void swap(Integer i,Integer j)
+	{
+         Integer temp = i;
+		 i = j;
+		 j = temp;
+	 System.out.println("i"+i+"  j "+j); 
+	}
+	public static void main(String[] args) 
+	{
+	   Integer a = new Integer(10);
+	   Integer b = new Integer(20);
+	   swap(a,b);
+	   System.out.println("i =jeelan "+a+", j fsdaf="+b);
+
+
+     }
+}
